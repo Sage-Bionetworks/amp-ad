e37be9b77dc2f162db2560cf96afccb0c1d727e9
mikeybkats <michaelbkats@gmail.com>
2018-06-26 17:20:22 -0700

    adding new amp ad logo

--- amp-ad-portal/src/Header.js
+++ amp-ad-portal/src/Header.js
@@ -1,48 +1,83 @@
-import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
- 
-import Dropdown from 'react-dropdown'
-import 'react-dropdown/style.css'
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 const options = [
-		{ label: <Link name="Programs" to="/Programs" className="nav-item dropdown">Programs</Link>, value: 'one' },
-		{ label: <Link name="Data Use" to="/DataUseRequirements" className="nav-item dropdown">Data Use</Link>, value: 'two' },
-		{ label: <Link name="Studies" to="/Studies" className="nav-item dropdown">Studies</Link>, value: 'three' }
-	]
+  {
+    label: (
+      <Link name="Programs" to="/Programs" className="nav-item dropdown">
+        Programs
+      </Link>
+    ),
+    value: "one"
+  },
+  {
+    label: (
+      <Link
+        name="Data Use"
+        to="/DataUseRequirements"
+        className="nav-item dropdown"
+      >
+        Data Use
+      </Link>
+    ),
+    value: "two"
+  },
+  {
+    label: (
+      <Link name="Studies" to="/Studies" className="nav-item dropdown">
+        Studies
+      </Link>
+    ),
+    value: "three"
+  }
+];
 
 //const defaultOption = options[0]
 
 const RouterDropDown = withRouter(({ history }) => (
-	<Dropdown 
-		options={options} 
-		onChange={
-			(event) => {
-				history.push(event.label.props.to)
-			}
-		}
-		placeholder="About" 
-	/>
-))
+  <Dropdown
+    options={options}
+    onChange={event => {
+      history.push(event.label.props.to);
+    }}
+    placeholder="About"
+  />
+));
 
 const Header = () => {
   return (
     <header className="row between-xs header center-xs middle-xs">
-          <div className="">
-            <Link to="/">
-							<img className="logo-header" src={require('./images/amp-ad-logo.svg')} alt="amp_ad_logo" />
-						</Link>
-          </div>
-          <div className="col-xs-8">
-            <ul className="nav row end-xs">
-              <li><Link to="/" className="nav-item active">Home</Link></li>
-              <li><Link to="/tools" className="nav-item">Tools</Link></li>
-							<li>
-								<RouterDropDown />	
-							</li>
-            </ul>
+      <div className="">
+        <Link to="/">
+          <img
+            className="logo-header"
+            src={require("./images/amp-ad-logo.svg")}
+            alt="amp_ad_logo"
+          />
+        </Link>
+      </div>
+      <div className="col-xs-8 col-sm-6">
+        <ul className="nav row end-xs">
+          <li>
+            <Link to="/" className="nav-item active">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/tools" className="nav-item">
+              Tools
+            </Link>
+          </li>
+          <li>
+            <RouterDropDown />
+          </li>
+        </ul>
       </div>
     </header>
   );
-}
+};
 
-export default Header
+export default Header;
