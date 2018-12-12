8138a780c5e0e298a5720b972c73e81e331712e8
mikeybkats <michaelbkats@gmail.com>
2018-12-12 14:32:03 -0800

    adding css overwrites for synapse react client hover states

--- amp-ad-portal/src/components/Page-Program.jsx
+++ amp-ad-portal/src/components/Page-Program.jsx
@@ -7,6 +7,8 @@ class ProgramPage extends Component {
     query: "",
     cardQuery: "",
     name: "",
+    wikiId: "",
+    synId: "",
   };
 
   componentDidMount() {
@@ -27,27 +29,39 @@ class ProgramPage extends Component {
   contentRouter = (handle = this.state.params.handle) => {
     let query
     let cardQuery
+    let wikiId
+    const synId = "syn12666371"
     switch (handle) {
     case "AMP-AD":
       query = "SELECT * FROM syn17024229 WHERE ( ( \"Program\" = 'AMP-AD' ) )"
       cardQuery = "SELECT * FROM syn17024173 where ( ( \"Program\" = 'AMP-AD' ) )"
+      wikiId = "581895"
       break
     case "M2OVE-AD":
       query = "SELECT * FROM syn17024229 WHERE ( ( \"Program\" = 'M2OVE-AD' ) )"
       cardQuery = "SELECT * FROM syn17024173 WHERE ( ( \"Program\" = 'M2OVE-AD' ) )"
+      wikiId = "581894"
       break
     case "MODEL-AD":
       query = "SELECT * FROM syn17024229 WHERE ( ( \"Program\" = 'MODEL-AD' ) )"
       cardQuery = "SELECT * FROM syn17024173 WHERE ( ( \"Program\" = 'MODEL-AD' ) )"
+      wikiId = "581896"
       break
     case "Resilience-AD":
       query = "SELECT * FROM syn17024229 WHERE ( ( \"Program\" = 'Resilience-AD' ) )"
       cardQuery = "SELECT * FROM syn17024173 WHERE ( ( \"Program\" = 'Resilience-AD' ) )"
+      wikiId = "581898"
       break
     default:
       query = "SELECT * FROM syn17024229"
     }
-    this.handleChanges({ query, cardQuery, name: handle })
+    this.handleChanges({
+      query,
+      cardQuery,
+      name: handle,
+      wikiId,
+      synId,
+    })
     return query
   };
 
@@ -86,9 +100,29 @@ class ProgramPage extends Component {
               type={this.props.SynapseConstants.AMP_CONSORTIUM}
             />
           </this.props.SynapseComponents.StaticQueryWrapper>
+          {this.returnWikiData()}
         </div>
       </div>
     )
+  };
+
+  returnWikiData = () => {
+    if (this.props.token) {
+      return (
+        <div className="container wiki-markdown">
+          <div className="row">
+            <div className="col-xs-12 col-sm-10 col-centered">
+              <this.props.SynapseComponents.Markdown
+                token={this.props.token}
+                ownerId={this.state.synId}
+                wikiId={this.state.wikiId}
+              />
+            </div>
+          </div>
+        </div>
+      )
+    }
+    return <div />
   };
 
   render() {
